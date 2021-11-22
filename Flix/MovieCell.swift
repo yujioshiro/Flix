ad0abd83f5633b734e025db2046fff049ea96e9d
Yuji Oshiro <Iamyujioshiro@gmail.com>
2021-11-21 19:16:25 -0800

    Complete Flix App

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by y on 11/20/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var posterView: UIImageView!
+    @IBOutlet var synopsisLabel: UILabel!
+    
+}
